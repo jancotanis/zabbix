48acd6fd8d48b2b88f1891adcb6178ca6a5eef48
Janco Tanis <git@jancology.com>
2024-02-15 14:08:49 +0100

    update docs

--- lib/zabbix/authentication.rb
+++ lib/zabbix/authentication.rb
@@ -3,12 +3,12 @@ module Zabbix
   # Deals with authentication flow and stores it within global configuration
   module Authentication
 
-    # https://helpcenter.veeam.com/docs/vac/rest/reference/vspc-rest.html?ver=80#tag/Authentication
-    # Authorize to the Veeam portal and return access_token
+    # Authorize to the Zabbix portal using the access_token
+    # @see https://www.zabbix.com/documentation/current/en/manual/api
     def login(options = {})
       raise ArgumentError, "Accesstoken/api-key not set" unless access_token
       # only bearer token needed 
-      # will do sanitty check if token if valid
+      # will do sanity check if token if valid
       rpc_call('settings.get')
     rescue RPCError => e
       raise AuthenticationError, e
